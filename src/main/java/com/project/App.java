c069e180e6a4c991e532081b2957b39caf0f5fdd
PineberryCode <applepie090902@gmail.com>
2023-07-21 21:23:20 -0500

    view

---
+++ src/main/java/com/project/App.java
@@ -0,0 +1,17 @@
+package com.project;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    public static void main (String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage arg0) throws Exception {
+        
+    }
+    
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,9 +2,5 @@ module com.project {
     requires javafx.controls;
     requires javafx.fxml;
 
-    opens com.project.view to javafx.fxml;
-
-    exports com.project.view to javafx.fxml;
-
     exports com.project to javafx.graphics;
 }
