6f84b495e536013af5fcd1c5e0839d56bd0c4d56
PineberryCode <applepie090902@gmail.com>
2023-07-27 09:44:06 -0500

    .gitignore

--- src/main/java/com/project/test/Testing.java
+++ src/main/java/com/project/test/Testing.java
@@ -2,6 +2,8 @@ package com.project.test;
 
 public class Testing {
     public static void main(String[] args) {
-        
+        double sin = Math.pow(Math.sin(10000), 2);
+        double cos = Math.pow(Math.cos(10000), 2);
+        System.out.println(sin+cos);
     }
 }
