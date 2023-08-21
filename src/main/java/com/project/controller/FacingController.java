b55bc441c877d56c3df3ef169e19dc54c4ced59d
PineberryCode <applepie090902@gmail.com>
2023-08-21 11:07:36 -0500

    21 August

--- src/main/java/com/project/controller/FacingController.java
+++ src/main/java/com/project/controller/FacingController.java
@@ -1,10 +1,12 @@
 package com.project.controller;
 
 import com.project.controller.process.FacingProcess;
-import com.project.database.crud.User_CRUD;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class FacingController {
 
@@ -15,6 +17,17 @@ public class FacingController {
      */
     @FXML
     private Label lblUsername;
+    @FXML
+    TextField input;
+    @FXML
+    TextField addFriend;
+    @FXML
+    TextArea txaConversation;
+
+    @FXML
+    private void addingFriend () {
+        System.out.println(addFriend.getText().toString());
+    }
 
     @FXML
     private void initialize () {
